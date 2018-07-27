a0a75ed363e2baf615399aa992c0aad08368855e
Mario Alejandro Montoya <mamcx@elmalabarista.com>
2018-07-26 21:58:52 -0500

    Fix file layout

--- lang/src/main.rs
+++ lang/src/main.rs
@@ -1,3 +1,11 @@
+mod core;
+
+use self::core::types::*;
+
 fn main() {
-    println!("Hello, world!");
+    let nums1 = (1..10).into_iter().collect();
+
+    let col1 = Column::from(nums1);
+
+    println!("Column {:?}", col1);
 }
