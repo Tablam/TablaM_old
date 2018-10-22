19d29d59ee23170323f493fff7e1d997e3037174
Mario Alejandro Montoya <mamcx@elmalabarista.com>
2018-10-21 20:20:17 -0500

    Moving files. All broken

--- drivers/src/lib.rs
+++ drivers/src/lib.rs
@@ -1,33 +1,3 @@
-extern crate rusqlite;
-
-use std::collections::HashMap;
-
-#[derive(PartialEq, Eq, Debug)]
-pub enum DbErr {
-    EngineError(String),
-    Connection,
-    Query,
-}
-
-#[derive(Debug, Clone)]
-pub enum Scalar {
-    None,
-    BOOL(bool),
-    I64(i64),
-    F64(f64),
-    UTF8(String),
-    Blob(Vec<u8>),
-}
-
-type DbRow = HashMap<String, Scalar>;
-type RowResult = Result<Vec<DbRow>, DbErr>;
-
-pub trait Rdbms {
-    fn select(&self, sql:&str, params:Option<DbRow>) -> RowResult;
-}
-
-mod sqlite;
-
 #[cfg(test)]
 mod tests {
     #[test]
